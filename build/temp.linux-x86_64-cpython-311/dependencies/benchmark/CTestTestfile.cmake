b9f75bef35ae99da296f35441e6e5e3c19a1e1dc
AgarCL <agarcl.2025@gmail.com>
2025-05-15 00:14:51 -0600

    removed build

--- build/temp.linux-x86_64-cpython-311/dependencies/benchmark/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ayman/thesis/AgarLE/dependencies/benchmark
-# Build directory: /home/ayman/thesis/AgarLE/build/temp.linux-x86_64-cpython-311/dependencies/benchmark
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("third_party/googletest/build")
-subdirs("test")
